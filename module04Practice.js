1c6f6cd8720f0299102d037bfb6dfc3c4d938124
Mayurparmar0507 <161591255+Mayurparmar0507@users.noreply.github.com>
2024-03-01 18:34:14 +0530

    module04Practice DOM file started

---
+++ module04Practice.js
@@ -0,0 +1,8 @@
+const p = document.querySelectorAll('div');
+const div = document.querySelectorAll('p');
+const img = document.querySelectorAll('.w3-content .w3-third img');
+//const img01 = img.querySelector('img');
+//console.log(img01);
+console.log(img);
+console.log(p);
+console.log(div);
