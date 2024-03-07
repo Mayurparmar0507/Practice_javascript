5f2a3941d9eee6eadccb66c6b6c4f6ecb6130ec6
Mayurparmar0507 <161591255+Mayurparmar0507@users.noreply.github.com>
2024-03-07 12:32:57 +0530

    updated practice

--- module04Practice.js
+++ module04Practice.js
@@ -1,8 +1,20 @@
 const p = document.querySelectorAll('div');
 const div = document.querySelectorAll('p');
 const img = document.querySelectorAll('.w3-content .w3-third img');
-//const img01 = img.querySelector('img');
-//console.log(img01);
-console.log(img);
-console.log(p);
-console.log(div);
+const header = document.querySelector('h1');
+// console.log(header.textContent); // get text contant from heading
+// console.log(header.innerText); // get text from css in header
+// console.log(header);
+// console.log(img);
+// console.log(p);
+// console.log(div);
+
+const cheking = document.querySelector('.chek');
+console.log(document.textContent);// Add the text available in tag and also hidden text also 
+
+console.log(cheking.innerHTML);
+console.log(cheking.innerText);
+cheking.textContent = 'chenge is applied';
+cheking.insertAdjacentText('afterend','new');
+console.log(cheking.textContent);
+
